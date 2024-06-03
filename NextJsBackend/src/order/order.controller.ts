546ca020fa2c8edd6301da84ed01a9f18d09271d
yousef sawy <yousefsawy2@gmail.com>
2024-06-03 16:42:18 +0300

    order_micro

---
+++ NextJsBackend/src/order/order.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { Order } from './order.model';
+
+@Controller('order')
+export class OrderController {
+    constructor(private readonly orderService: OrderService) { }
+
+    @Post()
+    async create(@Body() createOrderDto: Order): Promise<Order> {
+        return this.orderService.create(createOrderDto);
+    }
+
+    @Get()
+    async findAll(): Promise<Order[]> {
+        return this.orderService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<Order> {
+        return this.orderService.findOne(id);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() updateOrderDto: Order): Promise<Order> {
+        return this.orderService.update(id, updateOrderDto);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: string): Promise<any> {
+        return this.orderService.delete(id);
+    }
+}
---
+++ NextJsBackend/src/order/order.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Order } from './order.model';
+
+@Injectable()
+export class OrderService {
+    constructor(@InjectModel('Order') private readonly orderModel: Model<Order>) { }
+
+    async create(order: Order): Promise<Order> {
+        const newOrder = new this.orderModel(order);
+        return await newOrder.save();
+    }
+
+    async findAll(): Promise<Order[]> {
+        return await this.orderModel.find().exec();
+    }
+
+    async findOne(id: string): Promise<Order> {
+        return await this.orderModel.findById(id).exec();
+    }
+
+    async update(id: string, order: Order): Promise<Order> {
+        return await this.orderModel.findByIdAndUpdate(id, order, { new: true }).exec();
+    }
+
+    async delete(id: string): Promise<any> {
+        return await this.orderModel.deleteOne({ _id: id }).exec();
+    }
+}
